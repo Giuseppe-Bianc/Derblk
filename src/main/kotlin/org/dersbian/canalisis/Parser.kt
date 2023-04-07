bf933f52c793d54b0345c76531c5759571bedf8c
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2023-04-07 12:51:49 +0200

    aggiunto un blocco init e usa proprietà per nextToken

--- src/main/kotlin/org/dersbian/canalisis/Parser.kt
+++ src/main/kotlin/org/dersbian/canalisis/Parser.kt
@@ -1,12 +1,28 @@
 package org.dersbian.canalisis
 
 class Parser(inline val text: String) {
-    val tokens: List<Token> = Lexer(text).lex()
+    val tokens: List<Token>
     val _diagnostics: MutableList<String> = mutableListOf()
     var position = 0
 
+    init {
+        val lexer = Lexer(text)
+        tokens = lexer.lex()
+        _diagnostics.addAll(lexer.diagnostics)
+    }
+
     inline val diagnostics: List<String>
         inline get() = _diagnostics
+
+    inline val current: Token
+        inline get() = peek(0)
+
+    inline val nextToken: Token
+        inline get() {
+            val current = current
+            position++
+            return current
+        }
 
     inline fun peek(offset: Int): Token {
         val index = position + offset
@@ -15,22 +31,14 @@ class Parser(inline val text: String) {
         } else tokens[index]
     }
 
-    inline val current: Token
-        inline get() = peek(0)
-
-    inline fun nextToken(): Token {
-        val current = current
-        position++
-        return current
-    }
 
     inline fun matchToken(type: TokenType): Token {
         if (current.type === type) {
-            return nextToken()
+            return nextToken
         }
 
         _diagnostics.add("ERROR: Unexpected token <${current.type}>, expected <$type>")
-        return Token(type, current.position, null)
+        return Token(type, current.position, current.line)
     }
 
     inline fun parse(): SyntaxTree {
@@ -39,30 +47,14 @@ class Parser(inline val text: String) {
         return SyntaxTree(_diagnostics, expression, endOfFileToken)
     }
 
-    inline fun parseExpression(): Expression = parseTerm()
 
-    fun parseTerm(): Expression {
-        var left = parseFactor()
-
-        while (current.type === TokenType.PLUS ||
-            current.type === TokenType.MINUS
-        ) {
-            val operatorToken = nextToken()
-            val right = parseFactor()
-            left = BinaryExpression(left, operatorToken, right)
-        }
-
-        return left
-    }
-
-    inline fun parseFactor(): Expression {
-        var left = parsePrimaryExpression()
-
-        while (current.type === TokenType.MULTIPLY ||
-            current.type === TokenType.DIVIDE
-        ) {
-            val operatorToken = nextToken()
-            val right = parsePrimaryExpression()
+    fun parseExpression(parentPrecedence: Int = 0): Expression {
+        var left: Expression = parsePrimaryExpression()
+        while (true) {
+            val precedence = current.type.precedence
+            if (precedence == 0 || precedence <= parentPrecedence) break
+            val operatorToken = nextToken
+            val right = parseExpression(precedence)
             left = BinaryExpression(left, operatorToken, right)
         }
 
@@ -71,7 +63,7 @@ class Parser(inline val text: String) {
 
     inline fun parsePrimaryExpression(): Expression {
         return if (current.type === TokenType.LPAREN) {
-            val left = nextToken()
+            val left = nextToken
             val expression = parseExpression()
             val right = matchToken(TokenType.RPAREN)
             ParenthesizedExpression(left, expression, right)
