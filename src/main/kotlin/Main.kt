447dda3d7ae68a4f5a861a60245abfd9c72b2d03
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2023-04-07 12:53:09 +0200

    stampa risultato evaluator e l' AST

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
+import org.dersbian.canalisis.Evaluator
 import org.dersbian.canalisis.Expression
 import org.dersbian.canalisis.SyntaxTree
 
 private const val INPUT =
-        "(()( % 3 + 4 * 2 - ( 1 - 5 ) / 2 == != >= % <= < > = & && || % -22 | \r\n 2 + 0x33 \n inter = 077"
+    "(()( % 3 + 4 * 2 - ( 1 - 5 ) / 2 == != >= % <= < > = & && || % -22 | \r\n 2 + 0x33 \n inter = 077"
 private const val INPUTSS = INPUT + INPUT + INPUT + INPUT
 
 
 fun main() {
-    val expression = SyntaxTree.parse("1 - 5 + 4 + (2 + 3 + (0x33 + 077) + 1)")
+    val input = "1 - 5 + 4 + (2 + 3 + (0x33 + 077) / 2)"
+    val expression = SyntaxTree.parse("1 - 5 + 4 + (2 + 3 + (0x33 + 077) / 2)")
+    println("$input = ${Evaluator(expression.root).evaluate()}")
     Expression.prettyPrint(expression.root)
 }
